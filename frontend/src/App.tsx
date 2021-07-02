203765f20f8de6c1db7dbaa16d1e8d61d6368d77
Gus <gustavojo47@gmail.com>
2021-07-01 21:49:41 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/cadastro" component={Register} exact />
+        <Route path="/test" component={Navbar} exact />
         <Route path="/" component={Login} />
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Menubar } from 'primereact/menubar'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  const items = [
+    {
+      label: 'Cadastrar',
+      icon: 'pi pi-fw pi-plus',
+      items: [
+        {
+          label: 'Secretário',
+          icon: 'pi pi-fw pi-id-card'
+        },
+        {
+          label: 'Barbeiro',
+          icon: 'pi pi-fw pi-users'
+        },
+        {
+          label: 'Cliente',
+          icon: 'pi pi-fw pi-user-plus'
+        },
+        {
+          label: 'Atendimento',
+          icon: 'pi pi-fw pi-calendar-plus'
+        },
+        {
+          label: 'Corte',
+          icon: 'pi pi-fw pi-pencil'
+        }
+
+      ]
+    },
+    {
+      label: 'Agenda',
+      icon: 'pi pi-fw pi-calendar'
+    }
+  ]
+
+  const start = <img alt="logo" src="showcase/images/logo.png" height="40" className="p-mr-2"></img>
+  const end = <Link to="#" role="menuitem" className="p-menuitem-link" aria-haspopup="false" style={{ borderRadius: 5 }}>
+                <span className="p-menuitem-icon pi pi-fw pi-power-off"></span>
+                <span className="p-menuitem-text">Sair</span>
+                <span className="p-ink"></span>
+            </Link>
+
+  return (
+        <div>
+            <div className="card">
+                <Menubar model={items} start={start} end={end}/>
+            </div>
+        </div>
+  )
+}
+
+export default Navbar
